4cc8025b4d3ec3d9343326f610af2d13f5399721
Austin Lynn <waustinlynn@gmail.com>
2018-10-06 16:04:44 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fieldstone-men';
+export class AppComponent implements OnInit {
+  title = 'Fieldstone Men';
+  division1: any[];
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.division1 = [
+      {
+        "name": "first team",
+        "record": "2-1"
+      },
+      {
+        "name": "second team",
+        "record": "1-2"
+      }
+    ];
+    console.log(this.division1);
+  }
+
+  showData() {
+    console.log(this.division1);
+
+  }
 }
